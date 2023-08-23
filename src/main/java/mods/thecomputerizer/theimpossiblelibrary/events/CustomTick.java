636250373554051134ef6fb92bc60d918b45fe97
TheComputerizer <dcpixelation42@gmail.com>
2023-08-22 21:16:19 -0500

    CustomTick methods need to be Consumers rather than Suppliers

--- src/main/java/mods/thecomputerizer/theimpossiblelibrary/events/CustomTick.java
+++ src/main/java/mods/thecomputerizer/theimpossiblelibrary/events/CustomTick.java
@@ -3,7 +3,7 @@ package mods.thecomputerizer.theimpossiblelibrary.events;
 import java.util.*;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
-import java.util.function.Supplier;
+import java.util.function.Consumer;
 
 public class CustomTick {
 
@@ -12,14 +12,14 @@ public class CustomTick {
     /**
      * Adds a CustomTickEvent that fires every input number of times per second. The default vanilla tick rate is 20.
      */
-    public static void registerTickRate(int tickRate,Supplier<Long> eventSupplier) {
+    public static void registerTickRate(int tickRate,Consumer<Long> eventSupplier) {
         registerMillis((long)(1000f/tickRate),eventSupplier);
     }
 
     /**
      * Adds a CustomTickEvent that fires every input millisecond inverval. The default vanilla tick speed is 50 ms.
      */
-    public static void registerMillis(long millis,Supplier<Long> eventSupplier) {
+    public static void registerMillis(long millis,Consumer<Long> eventSupplier) {
         if(Objects.isNull(eventSupplier)) throw new RuntimeException("Cannot make a CustomTickEvent with a null supplier!");
         REGISTERED_TICKERS.getOrDefault(millis,makeNewTimer(millis)).addSupplier(eventSupplier);
     }
@@ -33,12 +33,14 @@ public class CustomTick {
                 "than 1000 ["+millis+"]!");
     }
 
-    private final List<Supplier<Long>> registeredSuppliers = Collections.synchronizedList(new ArrayList<>());
+    private final List<Consumer<Long>> registeredSuppliers = Collections.synchronizedList(new ArrayList<>());
+    private final long millis;
     private CustomTick(long millis) {
+        this.millis = millis;
         Executors.newScheduledThreadPool(1).scheduleAtFixedRate(this::run,0,millis,TimeUnit.MILLISECONDS);
     }
 
-    private void addSupplier(Supplier<Long> eventSupplier) {
+    private void addSupplier(Consumer<Long> eventSupplier) {
         synchronized (this.registeredSuppliers) {
             this.registeredSuppliers.add(eventSupplier);
         }
@@ -46,8 +48,8 @@ public class CustomTick {
 
     private void run() {
         synchronized (this.registeredSuppliers) {
-            for(Supplier<Long> supplier : this.registeredSuppliers)
-                supplier.get();
+            for(Consumer<Long> supplier : this.registeredSuppliers)
+                supplier.accept(this.millis);
         }
     }
 }
