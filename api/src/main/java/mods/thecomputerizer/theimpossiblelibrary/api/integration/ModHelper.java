7ee4f593c4d382407fbeec429d0dad892857f7c5
TheComputerizer <dcpixelation42@gmail.com>
2024-04-14 19:17:12 -0500

    Better ModHelper

--- api/src/main/java/mods/thecomputerizer/theimpossiblelibrary/api/integration/ModHelper.java
+++ api/src/main/java/mods/thecomputerizer/theimpossiblelibrary/api/integration/ModHelper.java
@@ -6,7 +6,32 @@ import mods.thecomputerizer.theimpossiblelibrary.api.core.TILRef;
 import javax.annotation.Nullable;
 import java.util.Objects;
 
+@SuppressWarnings("unused")
 public class ModHelper {
+
+    public static @Nullable BetterWeatherAPI betterWeather() {
+        return (BetterWeatherAPI)getModAPI(BetterWeatherAPI.MODID);
+    }
+
+    public static @Nullable BloodmoonAPI bloodmoon() {
+        return (BloodmoonAPI)getModAPI(BloodmoonAPI.MODID);
+    }
+
+    public static @Nullable ChampionsAPI champions() {
+        return (ChampionsAPI)getModAPI(ChampionsAPI.MODID);
+    }
+
+    public static @Nullable DynamicSurroundingsAPI dynamicSurroundings() {
+        return (DynamicSurroundingsAPI)getModAPI(DynamicSurroundingsAPI.MODID);
+    }
+
+    public static @Nullable EnhancedCelestialsAPI enhancedCelestials() {
+        return (EnhancedCelestialsAPI)getModAPI(EnhancedCelestialsAPI.MODID);
+    }
+
+    public static @Nullable GameStagesAPI gameStages() {
+        return (GameStagesAPI)getModAPI(GameStagesAPI.MODID);
+    }
 
     public static @Nullable ModAPI getModAPI(String modid) {
         ModHelperAPI api = getModHelperAPI();
@@ -22,8 +47,24 @@ public class ModHelper {
         return Objects.nonNull(api) ? api.getModName(modid) : null;
     }
 
+    public static @Nullable InfernalMobsAPI infernalMobs() {
+        return (InfernalMobsAPI)getModAPI(InfernalMobsAPI.MODID);
+    }
+
     public static boolean isModLoaded(String modid) {
         ModHelperAPI api = getModHelperAPI();
         return Objects.nonNull(api) && api.isModLoaded(modid);
+    }
+
+    public static @Nullable NyxAPI nyx() {
+        return (NyxAPI)getModAPI(NyxAPI.MODID);
+    }
+
+    public static @Nullable SereneSeasonsAPI sereneSeasons() {
+        return (SereneSeasonsAPI)getModAPI(SereneSeasonsAPI.MODID);
+    }
+
+    public static @Nullable Weather2API weather2() {
+        return (Weather2API)getModAPI(Weather2API.MODID);
     }
 }
