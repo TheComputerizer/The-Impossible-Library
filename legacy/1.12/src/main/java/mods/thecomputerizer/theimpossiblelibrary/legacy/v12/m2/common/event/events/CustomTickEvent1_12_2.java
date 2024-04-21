939d88577ecc808bf6eedf5353e794930813b44d
TheComputerizer <dcpixelation42@gmail.com>
2024-04-21 05:17:10 -0500

    Post custom tick event

--- legacy/1.12/src/main/java/mods/thecomputerizer/theimpossiblelibrary/legacy/v12/m2/common/event/events/CustomTickEvent1_12_2.java
+++ legacy/1.12/src/main/java/mods/thecomputerizer/theimpossiblelibrary/legacy/v12/m2/common/event/events/CustomTickEvent1_12_2.java
@@ -3,10 +3,18 @@ package mods.thecomputerizer.theimpossiblelibrary.legacy.v12.m2.common.event.eve
 import mods.thecomputerizer.theimpossiblelibrary.api.common.event.events.CustomTickEventWrapper;
 import mods.thecomputerizer.theimpossiblelibrary.api.util.CustomTick;
 import mods.thecomputerizer.theimpossiblelibrary.legacy.v12.m2.util.CustomTick1_12_2;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import java.util.Objects;
 
+import static mods.thecomputerizer.theimpossiblelibrary.api.common.event.CommonEventWrapper.CommonType.CUSTOM_TICK;
+
 public class CustomTickEvent1_12_2 extends CustomTickEventWrapper<CustomTick1_12_2> {
+
+    @SubscribeEvent
+    public static void onEvent(CustomTick1_12_2 event) {
+        CUSTOM_TICK.invoke(event);
+    }
 
     @Override
     protected CustomTick wrapTicker() {
