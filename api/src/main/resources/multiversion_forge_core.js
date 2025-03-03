d89beef94763a13240e03cbec8e084801709c6dd
TheComputerizer <dcpixelation42@gmail.com>
2025-03-03 12:58:49 -0600

    It finally works in all 1.16.5+ version

--- api/src/main/resources/multiversion_forge_core.js
+++ api/src/main/resources/multiversion_forge_core.js
@@ -10,31 +10,29 @@ function initializeCoreMod() {
     var instance = CORE.getInstance();
     log("Initializing "+instance.getCoreInstances().size()+" coremod(s)");
     var ret = {};
-    for each(let entryPoint in instance.getCoreInstances()) {
-        let id = entryPoint.getCoreID();
-        log("Initializing "+id);
-        ENTRYPOINTS[id] = entryPoint;
-        let targets = entryPoint.classTargets();
-        ret[id] = {
+    instance.getCoreInstances().forEach(function(entryPoint) {
+        log("Initializing "+entryPoint.getCoreID());
+        ENTRYPOINTS[entryPoint.getCoreID()] = entryPoint;
+        ret[entryPoint.getCoreID()] = {
             "target": {
             "type": "CLASS",
             "names": function(classMap) {
-                    let names = [];
-                    log("Handling targets for "+id);
-                    for each(let target in targets) {
-                        log("("+id+"): Pushing class target "+target);
-                        names.push(target)
-                    }
+                    var names = [];
+                    log("Handling targets for "+entryPoint.getCoreID());
+                    entryPoint.classTargets().forEach(function(target) {
+                        log("("+entryPoint.getCoreID()+"): Pushing class target "+target);
+                        names.push(target);
+                    });
                     return names;
                 }
             },
             "transformer": function(node) { //scope isn't constrained to the loop??
                 log("Transforming node "+node.name);
-                log("id = "+id);
-                ENTRYPOINTS[id].editClass(node)
+                log("id = "+entryPoint.getCoreID());
+                ENTRYPOINTS[entryPoint.getCoreID()].editClass(node);
                 return node;
             }
         }
-    }
+    });
     return ret;
 }
