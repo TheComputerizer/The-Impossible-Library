6e125dcb3c62bb1b189f8159115dd85bcf0ae15e
TheComputerizer <dcpixelation42@gmail.com>
2025-03-13 14:47:16 -0500

    enable org.gradle.parallel for much faster builds & figure out the right shadowJar order for neoforge

--- neoforge/1.20/1.20.6/src/main/java/mods/thecomputerizer/theimpossiblelibrary/neoforge/v20/m6/core/TILCoreNeoForge1_20_6.java
+++ neoforge/1.20/1.20.6/src/main/java/mods/thecomputerizer/theimpossiblelibrary/neoforge/v20/m6/core/TILCoreNeoForge1_20_6.java
@@ -3,11 +3,14 @@ package mods.thecomputerizer.theimpossiblelibrary.neoforge.v20.m6.core;
 import mods.thecomputerizer.theimpossiblelibrary.api.common.CommonEntryPoint;
 import mods.thecomputerizer.theimpossiblelibrary.api.core.TILRef;
 import mods.thecomputerizer.theimpossiblelibrary.api.core.annotation.IndirectCallers;
+import mods.thecomputerizer.theimpossiblelibrary.api.core.asm.ModWriter;
+import mods.thecomputerizer.theimpossiblelibrary.api.core.loader.MultiVersionModInfo;
 import mods.thecomputerizer.theimpossiblelibrary.neoforge.v20.core.TILCoreNeoForge1_20;
 import mods.thecomputerizer.theimpossiblelibrary.neoforge.v20.m6.client.ClientNeoForge1_20_6;
 import mods.thecomputerizer.theimpossiblelibrary.neoforge.v20.m6.client.TILClientEntryPointNeoForge1_20_6;
 import mods.thecomputerizer.theimpossiblelibrary.neoforge.v20.m6.common.CommonNeoForge1_20_6;
 import mods.thecomputerizer.theimpossiblelibrary.neoforge.v20.m6.common.TILCommonEntryPointNeoForge1_20_6;
+import mods.thecomputerizer.theimpossiblelibrary.neoforge.v20.m6.core.asm.ModWriterNeoForge1_20_6;
 
 @IndirectCallers
 public class TILCoreNeoForge1_20_6 extends TILCoreNeoForge1_20 {
@@ -22,6 +25,10 @@ public class TILCoreNeoForge1_20_6 extends TILCoreNeoForge1_20 {
     
     @Override public CommonEntryPoint getCommonVersionHandler() {
         return TILCommonEntryPointNeoForge1_20_6.getInstance();
+    }
+    
+    @Override protected ModWriter getModWriter(MultiVersionModInfo info) {
+        return new ModWriterNeoForge1_20_6(this,info);
     }
     
     @Override public void initAPI() {
---
+++ neoforge/1.20/1.20.6/src/main/java/mods/thecomputerizer/theimpossiblelibrary/neoforge/v20/m6/core/asm/ModWriterNeoForge1_20_6.java
@@ -0,0 +1,14 @@
+package mods.thecomputerizer.theimpossiblelibrary.neoforge.v20.m6.core.asm;
+
+import mods.thecomputerizer.theimpossiblelibrary.api.core.CoreAPI;
+import mods.thecomputerizer.theimpossiblelibrary.api.core.loader.MultiVersionModInfo;
+import mods.thecomputerizer.theimpossiblelibrary.neoforge.v20.core.asm.ModWriterNeoForge1_20;
+
+import static mods.thecomputerizer.theimpossiblelibrary.api.core.asm.ASMRef.JAVA21;
+
+public class ModWriterNeoForge1_20_6 extends ModWriterNeoForge1_20 {
+    
+    public ModWriterNeoForge1_20_6(CoreAPI core, MultiVersionModInfo info) {
+        super(core,info,JAVA21);
+    }
+}
