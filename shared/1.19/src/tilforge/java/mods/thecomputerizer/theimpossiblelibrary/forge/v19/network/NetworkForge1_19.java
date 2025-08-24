863f950b8664513dc51460db037a1d36dc05c0dc
TheComputerizer <dcpixelation42@gmail.com>
2025-08-24 17:22:28 -0500

    Forgot to push these

---
+++ shared/1.19/src/tilforge/java/mods/thecomputerizer/theimpossiblelibrary/forge/v19/network/NetworkForge1_19.java
@@ -0,0 +1,128 @@
+package mods.thecomputerizer.theimpossiblelibrary.forge.v19.network;
+
+import mods.thecomputerizer.theimpossiblelibrary.api.core.TILRef;
+import mods.thecomputerizer.theimpossiblelibrary.api.network.message.MessageAPI;
+import mods.thecomputerizer.theimpossiblelibrary.api.network.message.MessageDirectionInfo;
+import mods.thecomputerizer.theimpossiblelibrary.api.network.message.MessageWrapperAPI;
+import mods.thecomputerizer.theimpossiblelibrary.shared.v19.network.Network1_19;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.network.NetworkDirection;
+import net.minecraftforge.network.NetworkEvent.Context;
+import net.minecraftforge.network.NetworkRegistry.ChannelBuilder;
+import net.minecraftforge.network.simple.SimpleChannel;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Collection;
+import java.util.Objects;
+import java.util.function.Supplier;
+
+import static net.minecraftforge.network.NetworkDirection.LOGIN_TO_CLIENT;
+import static net.minecraftforge.network.NetworkDirection.LOGIN_TO_SERVER;
+import static net.minecraftforge.network.NetworkDirection.PLAY_TO_CLIENT;
+import static net.minecraftforge.network.NetworkDirection.PLAY_TO_SERVER;
+import static net.minecraftforge.network.PacketDistributor.PLAYER;
+
+public class NetworkForge1_19 extends Network1_19<SimpleChannel,NetworkDirection> {
+
+    private SimpleChannel network;
+
+    @Override public NetworkDirection getDirFromName(String name) {
+        return switch(name.toUpperCase()) {
+            case "LOGIN_TO_CLIENT" -> LOGIN_TO_CLIENT;
+            case "PLAY_TO_SERVER" -> PLAY_TO_SERVER;
+            case "LOGIN_TO_SERVER" -> LOGIN_TO_SERVER;
+            default -> PLAY_TO_CLIENT;
+        };
+    }
+
+    @Override public String getNameFromDir(NetworkDirection dir) {
+        return dir.name();
+    }
+
+    @Override public NetworkDirection getDirToClient() {
+        return PLAY_TO_CLIENT;
+    }
+
+    @Override public NetworkDirection getDirToClientLogin() {
+        return LOGIN_TO_CLIENT;
+    }
+
+    @Override public NetworkDirection getDirToServer() {
+        return PLAY_TO_SERVER;
+    }
+
+    @Override public NetworkDirection getDirToServerLogin() {
+        return LOGIN_TO_SERVER;
+    }
+
+    @Override public @Nullable NetworkDirection getOppositeDir(NetworkDirection dir) {
+        return switch(dir) {
+            case PLAY_TO_CLIENT -> PLAY_TO_SERVER;
+            case PLAY_TO_SERVER -> PLAY_TO_CLIENT;
+            case LOGIN_TO_CLIENT -> LOGIN_TO_SERVER;
+            case LOGIN_TO_SERVER -> LOGIN_TO_CLIENT;
+        };
+    }
+
+    @Override public SimpleChannel getNetwork() {
+        if(Objects.isNull(this.network))
+            this.network = ChannelBuilder.named(TILRef.res("main_network").unwrap())
+                    .clientAcceptedVersions(version -> true)
+                    .serverAcceptedVersions(version -> true)
+                    .networkProtocolVersion(TILRef::getNetworkVersion)
+                    .simpleChannel();
+        return this.network;
+    }
+
+    @Override public boolean isDirToClient(NetworkDirection dir) {
+        return dir==PLAY_TO_CLIENT || dir==LOGIN_TO_CLIENT;
+    }
+
+    @Override public boolean isDirLogin(NetworkDirection dir) {
+        return dir==LOGIN_TO_CLIENT || dir==LOGIN_TO_SERVER;
+    }
+
+    @Override public void registerMessage(MessageDirectionInfo<NetworkDirection> dir, int id) {
+        final Class<MessageWrapperAPI<Player,Context>> msgClass = MessageWrapperAPI.getClass(dir);
+        getNetwork().messageBuilder(msgClass,id)
+                .encoder(MessageWrapperAPI.encoder()).decoder(MessageWrapperAPI.decoder(dir))
+                .consumer(MessageWrapperAPI.handler(dir, Supplier::get, Context::getSender))
+                .add();
+    }
+    
+    //TODO Does not support login direction
+    @Override public <P,M extends MessageWrapperAPI<?,?>> void sendToPlayer(M message, P player) {
+        getNetwork().send(PLAYER.with(() -> (ServerPlayer)player),message);
+    }
+    
+    //TODO Does not support login direction
+    @Override public <M extends MessageWrapperAPI<?,?>> void sendToServer(M message) {
+        getNetwork().sendToServer(message);
+    }
+    
+    @Override public <CTX> MessageWrapperAPI<?,CTX> wrapMessage(NetworkDirection dir, MessageAPI<CTX> message) {
+        MessageWrapperAPI<?,CTX> wrapper = MessageWrapperAPI.getInstance(dir);
+        if(Objects.nonNull(wrapper)) wrapper.setMessage(dir,message);
+        else TILRef.logError("Null message wrapper for dir {}",dir);
+        return wrapper;
+    }
+    
+    
+    @SafeVarargs
+    @Override public final <CTX> MessageWrapperAPI<?,CTX> wrapMessages(NetworkDirection dir,
+            MessageAPI<CTX> ... messages) {
+        MessageWrapperAPI<?,CTX> wrapper = MessageWrapperAPI.getInstance(dir);
+        if(Objects.nonNull(wrapper)) wrapper.setMessages(dir,messages);
+        else TILRef.logError("Null message wrapper for dir {}",dir);
+        return wrapper;
+    }
+    
+    @Override public <CTX> MessageWrapperAPI<?,CTX> wrapMessages(NetworkDirection dir,
+            Collection<MessageAPI<CTX>> messages) {
+        MessageWrapperAPI<?,CTX> wrapper = MessageWrapperAPI.getInstance(dir);
+        if(Objects.nonNull(wrapper)) wrapper.setMessages(dir,messages);
+        else TILRef.logError("Null message wrapper for dir {}",dir);
+        return wrapper;
+    }
+}
---
+++ shared/1.21/1.21.1/src/tilfabric/java/mods/thecomputerizer/theimpossiblelibrary/fabric/v21/m1/integration/EnhancedCelestialsFabric1_21_1.java
@@ -0,0 +1,49 @@
+package mods.thecomputerizer.theimpossiblelibrary.fabric.v21.m1.integration;
+
+import dev.corgitaco.enhancedcelestials.EnhancedCelestials;
+import dev.corgitaco.enhancedcelestials.api.lunarevent.LunarEvent;
+import dev.corgitaco.enhancedcelestials.lunarevent.EnhancedCelestialsLunarForecastWorldData;
+import mods.thecomputerizer.theimpossiblelibrary.api.core.Hacks;
+import mods.thecomputerizer.theimpossiblelibrary.api.integration.EnhancedCelestialsAPI;
+import mods.thecomputerizer.theimpossiblelibrary.api.world.WorldAPI;
+import net.minecraft.core.Holder;
+import net.minecraft.resources.ResourceKey;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Objects;
+import java.util.Optional;
+
+public class EnhancedCelestialsFabric1_21_1 extends EnhancedCelestialsAPI {
+
+    public @Nullable EnhancedCelestialsLunarForecastWorldData getContext(WorldAPI<?> api) {
+        return orElseNull(Hacks.invokeStatic(EnhancedCelestials.class,"lunarForecastWorldData",api.unwrap()));
+    }
+
+    @Override public boolean isBloodMoon(WorldAPI<?> world) {
+        return isEvent(world,"bloodmoon");
+    }
+
+    @Override public boolean isBlueMoon(WorldAPI<?> world) {
+        return isEvent(world,"bluemoon");
+    }
+
+    public boolean isEvent(WorldAPI<?> world, String type) {
+        EnhancedCelestialsLunarForecastWorldData context = getContext(world);
+        if(Objects.nonNull(context)) {
+            Holder<LunarEvent> event = Hacks.invoke(context,"currentLunarEventHolder");
+            Optional<ResourceKey<LunarEvent>> optional = Objects.nonNull(event) ? event.unwrapKey() : Optional.empty();
+            if(optional.isEmpty()) return false;
+            ResourceKey<LunarEvent> key = optional.get();
+            return key.registry().toString().contains(type);
+        }
+        return false;
+    }
+
+    @Override public boolean isHarvestMoon(WorldAPI<?> world) {
+        return isEvent(world,"harvestmoon");
+    }
+
+    @Override public boolean isMoon(WorldAPI<?> world) {
+        return isEvent(world,":moon");
+    }
+}
